7658920ae519a7c3689f979a84afb04528ff53a1
Milo Adams <adamsl9@mymail.nku.edu>
2024-09-19 11:38:02 -0400

    Update detail.php

--- project/detail.php
+++ project/detail.php
@@ -83,8 +83,7 @@
 			$new_line=[$line[0],++$line[1]];
 			$line = $new_line;
 		}
-		array_push($new_content, $line);
-	}
+		array_push($new_content, $line);}
 	fclose($f);
 	$w = fopen($file, "w");
 	for (x=0, x<count($new_content), x++){
@@ -93,7 +92,7 @@
 	
 	echo "Number of Views: ".$second_line[1];
 	fclose($w);
-    ?>
+  ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
